8accc7db97ed4cc03e44153c9121f78fc8146815
sanjaynishad355 <134122153+sanjaynishad355@users.noreply.github.com>
2023-12-14 22:01:11 +0530

    Add files via upload

---
+++ 01 Python Intro.py
@@ -0,0 +1,19 @@
+# print("pooja")
+# print("pooja,heena")
+# print("pooja","heena")
+# print("pooja",",","heena")
+
+print("--------------------")
+
+print("calculator program")
+a=1
+b=0
+print("a =",a)
+print("b =",b)
+print("Addition =",a+b)
+print("Subtraction =",a-b)
+print("Multiplication =",a*b)
+# print("Division =",a/b) # zero division error
+# print("Modulas =",a%b) # zero division error
+# print("Floor Division =",a//b) # zero division error
+print("a power of b =",a**a*b)
